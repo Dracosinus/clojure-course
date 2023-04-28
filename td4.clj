e246800d61ad71c2b9c9642c885574116fbe2b25
Benoit SCHULER <benoit.schuler@wisper.io>
2023-04-28 12:06:07 +0200

    Printing trees

--- td4.clj
+++ td4.clj
@@ -3,13 +3,13 @@
 (defrecord Node [value left right])
 
 (defn a-vide []
-  (map->Node {:value nil :left nil :right nil}))
+  (->Node nil nil nil))
 
 
 (a-vide)
 
 (defn a-noeud [v sag sad]
-  (map->Node {:value v :left sag :right sad}))
+  (->Node v sag sad))
 
 (def tree0 (a-noeud 0 (a-vide) (a-vide)))
 
@@ -33,6 +33,34 @@
 (a-gauche (a-vide))
 (a-gauche (a-noeud 0 tree0 (a-vide)))
 
-(defn a-right [{:keys [right]}]
-  right)
+(defn a-right [node]
+  (:right node))
 
+(defn a-print [A]
+  (defn a-print-depth [A depth]
+    (cond
+      (a-vide? A) (println (str (apply str (repeat depth "\t")) "()"))
+      :else (let [{val :value sag :left sad :right} A]
+              (println (str (apply str (repeat depth "\t")) val))
+              (a-print-depth sag (inc depth))
+              (a-print-depth sad (inc depth)))))
+  (a-print-depth A 0)
+  )
+
+(a-print (a-vide))
+(a-print tree0)
+(def A (a-noeud 1 (a-noeud 2 (a-vide) (a-noeud 3 (a-vide) (a-vide))) (a-noeud 4 (a-vide)
+                                                                                (a-vide))))
+(a-print A)
+
+(defn a-print-inf [A]
+  (defn a-print-depth [A depth]
+    (cond
+      (a-vide? A) (println (str (apply str (repeat depth "\t")) "()"))
+      :else (let [{val :value sag :left sad :right} A] 
+              (a-print-depth sag (inc depth))
+              (println (str (apply str (repeat depth "\t")) val))
+              (a-print-depth sad (inc depth)))))
+  (a-print-depth A 0))
+
+(a-print-inf A)
