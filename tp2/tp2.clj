dbd29894048f5c2ec888b92fc15da98dd20e86d2
Benoit SCHULER <benoit.schuler@wisper.io>
2023-05-12 11:07:36 +0200

    Refactor until print-book

--- tp2/tp2.clj
+++ tp2/tp2.clj
@@ -7,49 +7,45 @@
 (def emprunts (atom {}))
 
 ;1
-(defrecord Book [titre auteur éditeur
+(defrecord Book [titre auteur editeur
 date-de-publication isbn exemplaires-disponibles])
 
-(defn create-book [titre auteur éditeur
+(defn create-book [titre auteur editeur
 date-de-publication isbn]
-  (swap! bibliotheque assoc isbn (->Book titre auteur éditeur
+  (swap! bibliotheque assoc isbn (->Book titre auteur editeur
 date-de-publication isbn 1))
 )
 
 (create-book "harry potter a l'école des sorciers" "JK Rowling"
-             "Gallimard jeunesse" (t/date-time 1997 06 26) 2070584623)
-(create-book "harry potter a l'école des sorciers" "JK Rowling"
-             "Gallimard jeunesse" (t/date-time 1997 07 02) 2070584624)
-(create-book "L'appel de Cthulhu" "HP Lovecraft" "POINTS" (t/date-time 1928 02) 2757851357)
-(def hp1 (find @bibliotheque 2070584623))
-(def hp2 (find @bibliotheque 2070584624))
-(def cthulhu (find @bibliotheque 2757851357))
+             "Gallimard jeunesse" (t/date-time 1997 06 26) "2070584623")
+(create-book "harry potter et la chambre des secrets" "JK Rowling"
+             "Gallimard jeunesse" (t/date-time 1997 07 02) "2070584624")
+(create-book "L'appel de Cthulhu" "HP Lovecraft" "POINTS" (t/date-time 1928 02) "2757851357")
 
 
-(def test-book-list [hp1 cthulhu hp2])
+(defn list-books-by-author [author]
+  (filter #(= author (:auteur %)) (vals @bibliotheque)))
 
-(defn list-books-by-author [books author]
-  (filter #(= author (:auteur %)) books))
+(list-books-by-author "JK Rowling")
 
-(list-books-by-author test-book-list "JK Rowling")
-
-(defn sort-books-by-date [books]
-  (sort-by :date-de-publication books)
+(defn sort-books-by-date []
+  (sort-by :date-de-publication (vals @bibliotheque))
 )
 
-(sort-books-by-date test-book-list)
+(sort-books-by-date)
 
 (defn get-book-isbn [book] 
   (:isbn book))
 
-(get-book-isbn cthulhu)
+(get-book-isbn (get @bibliotheque "2070584623"))
+
 
 (defn set-book-isbn [book isbn] 
   (swap! bibliotheque dissoc (:isbn book))
   (swap! bibliotheque assoc isbn (assoc book :isbn isbn))
   )
 
-;(get-book-isbn (set-book-isbn cthulhu 1234))
+(get-book-isbn (set-book-isbn (get @bibliotheque "2070584623") 1234))
 
 (defn print-book [book]
   (let [{titre :titre auteur :auteur editeur :editeur date-de-publication :date-de-publication isbn :isbn exemplaires-disponibles :exemplaires-disponibles} book]
@@ -62,8 +58,7 @@ date-de-publication isbn 1))
     )
 )
 
-(print-book cthulhu)
-
+(print-book (get @bibliotheque "2070584623"))
 
 (defrecord Borrower [nom prenom addresse livres-empruntes])
 
